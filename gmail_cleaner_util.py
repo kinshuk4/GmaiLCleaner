47e1f534abd1679bdf08ccaef00458604e2cfd2a
Kinshuk <kinshuk.ram@gmail.com>
2017-06-23 13:31:05 +0200

    Making the main code cleaner

--- gmail_cleaner_util.py
+++ gmail_cleaner_util.py
@@ -1,3 +1,9 @@
+import os
+import re
+
+import config as conf
+import url_util as uu
+from main import DEBUG
 
 
 def get_multi_level_val_from_dict(dct, path_str, default=None):
@@ -31,3 +37,89 @@ def rget(dct, keys, default=None):
     if not keys:
         return elem
     return rget(elem, keys, default)
+
+
+def extract_urls_from_body(body):
+    # sender_set, header_set = getPocketSenders()
+    urls = set()
+
+    # print(subject)
+    str_body = str(body)
+    searched_urls = re.findall(conf.URL_REGEX, str_body)
+    for url in searched_urls:
+        if not any(ignore_url in url.lower() for ignore_url in conf.IGNORE_URLS):
+            urls.add(url)
+        else:
+            pass
+            # print(url)
+
+    # cleanup URLs
+    cleanedup_urls = set()
+    if DEBUG:
+        print("+++++++++++++++++++++++++ Got URL Set 1 +++++++++++++++++++++++++++")
+    for url in urls:
+        url_lower = url.lower()
+        if DEBUG:
+            print(url_lower)
+
+        for x in conf.characters_exclusion:
+            if x in url_lower:
+                url_lower = url_lower.replace(x, "")
+                cleanedup_urls.add(url_lower)
+            else:
+                cleanedup_urls.add(url_lower)
+
+    # starting second set of rules
+    all_urls_valid_for_pocket = []
+    if DEBUG:
+        print("+++++++++++++++++++++++++ Got URL Set 2 +++++++++++++++++++++++++++")
+    for url in cleanedup_urls:
+        # url needs encoding
+        if DEBUG:
+            print(url)
+        if ';' in url:
+            url = uu.decode_url(url)
+            # TODO for some reason decoding is not working and not replacing &amp; to &
+            url = url.replace('&amp;', '&')
+        if check_url(url):
+            all_urls_valid_for_pocket.append(url)
+    return all_urls_valid_for_pocket
+
+
+def check_url(url):
+    url_lower = url.lower()
+    url_without_query_param = uu.remove_query_params(url_lower)
+    extension = os.path.splitext(url_without_query_param)[1]
+
+    if extension in conf.extensions_to_exclude:
+        return False
+
+    # if any(x in url_lower for x in conf.characters_exclusion):
+    #     return False
+    if url_lower is None:
+        print(url_lower)
+    if uu.check_if_only_domain_name(url_lower):
+        return False
+    return True
+
+
+def get_mailbody_without_footer(mail_body):
+    mail_body_str = str(mail_body)
+    for x in conf.footer_finder_strings:
+        if x in mail_body_str:
+            mail_without_footer = mail_body_str.split(x)[0]
+            return mail_without_footer
+    return mail_body_str
+
+
+def get_mailbody_without_footer_careful(mail_body):
+    mail_body_str = str(mail_body)
+    body_length = len(mail_body_str)
+    for x in conf.careful_footer_strings:
+        if x in mail_body_str:
+            location = mail_body_str.find(x)
+            if location > 0.6 * body_length:
+                mail_without_footer = mail_body_str.split(x)[0]
+                return mail_without_footer
+
+    return mail_body_str
--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-from pocket import Pocket, PocketException
 # Importing required libraries
-from apiclient import discovery
-from apiclient import errors
-from httplib2 import Http
-from oauth2client import file, client, tools
-import base64
-from bs4 import BeautifulSoup
-import re
-import time
 # import dateutil.parser as parser
-from datetime import datetime
-import datetime
-import csv
+import uuid
+
+import config as conf
+from gmail_cleaner_util import extract_urls_from_body, get_mailbody_without_footer
 from gmail_client import PythonGmailAPI
 from pocket_client import PythonPocketAPI
-import config as conf
-import re
-import uuid
-import os
-import url_util as uu
 
 '''
 This script does the following:
@@ -35,92 +22,6 @@ def exclude_message_on_subject(subject_string):
     if any(header.lower() in subject_string.lower() for header in conf.HEADERS_TO_EXCLUDE):
         return True
     return False
-
-
-def extract_urls_from_body(body):
-    # sender_set, header_set = getPocketSenders()
-    urls = set()
-
-    # print(subject)
-    str_body = str(body)
-    searched_urls = re.findall(conf.URL_REGEX, str_body)
-    for url in searched_urls:
-        if not any(ignore_url in url.lower() for ignore_url in conf.IGNORE_URLS):
-            urls.add(url)
-        else:
-            pass
-            # print(url)
-
-    # cleanup URLs
-    cleanedup_urls = set()
-    if DEBUG:
-        print("+++++++++++++++++++++++++ Got URL Set 1 +++++++++++++++++++++++++++")
-    for url in urls:
-        url_lower = url.lower()
-        if DEBUG:
-            print(url_lower)
-
-        for x in conf.characters_exclusion:
-            if x in url_lower:
-                url_lower = url_lower.replace(x, "")
-                cleanedup_urls.add(url_lower)
-            else:
-                cleanedup_urls.add(url_lower)
-
-    # starting second set of rules
-    all_urls_valid_for_pocket = []
-    if DEBUG:
-        print("+++++++++++++++++++++++++ Got URL Set 2 +++++++++++++++++++++++++++")
-    for url in cleanedup_urls:
-        # url needs encoding
-        if DEBUG:
-            print(url)
-        if ';' in url:
-            url = uu.decode_url(url)
-            # TODO for some reason decoding is not working and not replacing &amp; to &
-            url = url.replace('&amp;', '&')
-        if check_url(url):
-            all_urls_valid_for_pocket.append(url)
-    return all_urls_valid_for_pocket
-
-
-def check_url(url):
-    url_lower = url.lower()
-    url_without_query_param = uu.remove_query_params(url_lower)
-    extension = os.path.splitext(url_without_query_param)[1]
-
-    if extension in conf.extensions_to_exclude:
-        return False
-
-    # if any(x in url_lower for x in conf.characters_exclusion):
-    #     return False
-    if url_lower is None:
-        print(url_lower)
-    if uu.check_if_only_domain_name(url_lower):
-        return False
-    return True
-
-
-def get_mailbody_without_footer(mail_body):
-    mail_body_str = str(mail_body)
-    for x in conf.footer_finder_strings:
-        if x in mail_body_str:
-            mail_without_footer = mail_body_str.split(x)[0]
-            return mail_without_footer
-    return mail_body_str
-
-
-def get_mailbody_without_footer_careful(mail_body):
-    mail_body_str = str(mail_body)
-    body_length = len(mail_body_str)
-    for x in conf.careful_footer_strings:
-        if x in mail_body_str:
-            location = mail_body_str.find(x)
-            if location > 0.6 * body_length:
-                mail_without_footer = mail_body_str.split(x)[0]
-                return mail_without_footer
-
-    return mail_body_str
 
 
 print("Initializing the gmail api")
